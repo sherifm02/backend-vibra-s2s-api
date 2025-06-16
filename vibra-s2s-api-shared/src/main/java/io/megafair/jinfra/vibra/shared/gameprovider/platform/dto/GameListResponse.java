0b08e2609ef90e95e5ec6727d8411bc733b48422
sharif <sherif.huseynli@megafair.io>
2025-06-17 00:48:40 +0400

    refinements

--- vibra-s2s-api-shared/src/main/java/io/megafair/jinfra/vibra/shared/gameprovider/platform/dto/GameListResponse.java
+++
@@ -1,7 +0,0 @@
-package io.megafair.jinfra.vibra.shared.gameprovider.platform.dto;
-
-import jakarta.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name="GAMESSUITES")
-public class GameListResponse {
-}
